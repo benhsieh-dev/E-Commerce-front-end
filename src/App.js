279fe2719355d6141ccb6c431b8cd1b8935ec4f8
Ben Hsieh <bqh5026@gmail.com>
2024-09-29 22:40:51 -0500

    Add navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
+import Navigation from './components/Navigation/Navigation.jsx'
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className='App'>
+      <Navigation />
+    </div>
+  )
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className="flex items-center py-6 px-8 justify-between">
+      <div className=''>
+        {/* logo */}
+        <h2 className='text-3xl text-black'>New Fully Bakery, Inc.</h2>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation
