3728cc882dd84b4063717e6a2f987924ccb9a5e4
Ben Hsieh <bqh5026@gmail.com>
2024-09-30 20:59:04 -0500

    Add navigation items

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,10 +2,29 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <nav className="flex items-center py-6 px-8 justify-between">
-      <div className=''>
+    <nav className="flex items-center py-6 px-16 justify-between">
+      <div className="flex items-center gap-6">
         {/* logo */}
-        <h2 className='text-3xl text-black'>New Fully Bakery, Inc.</h2>
+        <h2 className="text-3xl text-black">New Fully Bakery, Inc.</h2>
+      </div>
+      <div className="flex flex-wrap items-center gap-8">
+        <ul className="flex">
+          <li>
+            <a href="/">breads</a>
+          </li>
+          <li>
+            <a href="/">cakes</a>
+          </li>
+          <li>
+            <a href="/">cookies</a>
+          </li>
+          <li>
+            <a href="/">mooncakes</a>
+          </li>
+          <li>
+            <a href="/">specialized cakes</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
