8ef69e59b1716866da2629c0df4823d5060b11c9
Ben Hsieh <bqh5026@gmail.com>
2024-10-02 21:16:55 -0500

    Update css on navigation items

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,9 +5,9 @@ const Navigation = () => {
     <nav className="flex items-center py-6 px-16 justify-between">
       <div className="flex items-center gap-6">
         {/* logo */}
-        <h2 className="text-3xl text-black">New Fully Bakery, Inc.</h2>
+        <a className="text-3xl text-black gap-8" href='/'>New Fully Bakery, Inc.</a>
       </div>
-      <div className="flex flex-wrap items-center gap-8">
+      <div className="flex flex-wrap items-center gap-10 flex-1">
         <ul className="flex">
           <li>
             <a href="/">breads</a>
