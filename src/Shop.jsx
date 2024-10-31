232eded12ea23efca008ee34f45e289019771b33
Ben Hsieh <bqh5026@gmail.com>
2024-10-30 23:34:04 -0500

    Add hero section to main page

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,10 +1,12 @@
 import './Shop.css';
+import HeroSection from './components/HeroSection/HeroSection.jsx';
 import Navigation from './components/Navigation/Navigation.jsx'
 
 export default function Shop() {
   return (
     <div className='App'>
       <Navigation />
+      <HeroSection />
     </div>
   )
 }
