85ef1b4e84eb779bba971e7631d1871461f62e9a
Ben Hsieh <bqh5026@gmail.com>
2024-11-03 23:14:28 -0600

    Add product category

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -12,9 +12,12 @@ const HeroSection = () => {
         <div className="text-left">
           <h2 className="text-2xl text-white">Cakes</h2>
         </div>
-        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-6xl">Mooncakes</p>
+        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-6xl">
+          Mooncakes
+        </p>
         <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl">Breads</p>
         <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl">Cookies</p>
+        <p className="mt-3 text-white sm:mt-5 sm:max-w-xl text-2xl">Drinks</p>
         <button className="border rounded mt-6 border-black hover:bg-white hover:text-black hover:border-black text-white bg-black w-44 h-12">
           Shop Now
         </button>
