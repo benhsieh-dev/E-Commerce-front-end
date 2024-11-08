0de56f12d1aa9005b94c089f04b4e84573a8f723
Ben Hsieh <bqh5026@gmail.com>
2024-11-07 23:06:34 -0600

    Update section heading component

---
+++ src/components/Sections/SectionHeading/SectionHeading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SectionHeading = () => {
+  return (
+    <div className='flex flex-wrap justify-around'>
+        <div>
+        </div>     
+        <p>Heading</p>
+    </div>
+  )
+}
+
+export default SectionHeading
