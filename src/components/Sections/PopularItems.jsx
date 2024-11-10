bedaa0a36b3bab43fbb5869765914066a49afeb5
Ben Hsieh <bqh5026@gmail.com>
2024-11-09 20:59:56 -0600

    Update components

---
+++ src/components/Sections/PopularItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from './SectionHeading/SectionHeading'
+
+const PopularItems = () => {
+  return (
+    <div>
+      <SectionHeading title={'Popular Items'} />
+    </div>
+  )
+}
+
+export default PopularItems
--- src/components/Sections/SectionHeading/SectionHeading.jsx
+++ src/components/Sections/SectionHeading/SectionHeading.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
-const SectionHeading = () => {
+const SectionHeading = ({title}) => {
   return (
     <div className='flex flex-wrap justify-around'>
         <div>
         </div>     
-        <p>Heading</p>
+        <p>{title}</p>
     </div>
   )
+}
+
+SectionHeading.defaultProps = {
+
+}
+
+SectionHeading.protoTypes = {
+  title:String
 }
 
 export default SectionHeading
