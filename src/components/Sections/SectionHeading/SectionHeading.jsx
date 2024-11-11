dbea2670c8295ff1708ec3bf5cf39eb45d64a560
Ben Hsieh <bqh5026@gmail.com>
2024-11-10 23:21:45 -0600

    Update section heading component

--- src/components/Sections/SectionHeading/SectionHeading.jsx
+++ src/components/Sections/SectionHeading/SectionHeading.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SectionHeading = ({title}) => {
   return (
-    <div className='flex flex-wrap justify-around'>
-        <div>
+    <div className='flex flex-wrap px-10 my-5 items-center gap-2'>
+        <div className='border rounded border-1 bg-black w-2 h-10'>
         </div>     
-        <p>{title}</p>
+        <p className='text-2xl'>{title}</p>
     </div>
   )
 }
