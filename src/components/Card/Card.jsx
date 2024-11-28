2e158dc0e3c4d8a4bd18db1a94a7cc93d3df2930
Ben Hsieh <bqh5026@gmail.com>
2024-11-27 23:18:22 -0600

    Update card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,10 @@ import Display from '../../assets/img/display.jpg';
 
 const Card = () => {
   return (
-    <div className='flex items-center flex-col'>
-        <img src={Display} height={"320px"} width={"420px"} alt="Display" />
+    <div className="flex flex-col p-8">
+      <img className='max-h-[260px] max-w-[240px] bg-cover bg-center border rounded' src={Display} alt="Display" />
     </div>
-  )
+  );
 }
 
 export default Card
